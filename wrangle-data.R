812eca697beab0655fafc405498ee5de4a851f3f
Roja Kasula <roja_kasula@yahoo.com>
2022-02-22 20:39:11 -0500

    adding all files

---
+++ wrangle-data.R
@@ -0,0 +1,8 @@
+library(tidyverse)
+murders <- read_csv("data/murders.csv")
+head(murders)
+murders <- murders %>% mutate(region = factor(region), rate= total/population * 10^5)
+head(murders)
+save(murders,file = "rda/murders.rda")
+
+
